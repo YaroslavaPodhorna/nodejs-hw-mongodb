00759f1f8ef8d63e04210ae45c96170cfc797850
Yaroslava <yariii4ek@gmail.com>
2025-05-14 23:41:34 -0700

    hw-3

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,20 @@
+import { HttpError } from 'http-errors';
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      status: err.status,
+      message: err.message,
+      data: err,
+    });
+  }
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+};
+// {
+// 		status: 500,
+// 		message: "Something went wrong",
+// 		data:
+// 		// конкретне повідомлення про помилку, отримане з об'єкта помилки
+// }
